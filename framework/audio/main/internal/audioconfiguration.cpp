musescore/MuseScore

Set default audio API to CoreAudio on macOS

src/framework/audio/main/internal/audioconfiguration.cpp
@@ -63,6 +63,8 @@ void AudioConfiguration::init()
     settings()->setDefaultValue(AUDIO_API_KEY, Val("WASAPI"));
 #elif defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
     settings()->setDefaultValue(AUDIO_API_KEY, Val("ALSA"));
+#elif defined(Q_OS_MACOS)
+    settings()->setDefaultValue(AUDIO_API_KEY, Val("CoreAudio"));
 #endif
     settings()->valueChanged(AUDIO_API_KEY).onReceive(nullptr, [this](const Val&) {
         m_currentAudioApiChanged.notify();

musescore/muse_framework

framework/audio/main/internal/audioconfiguration.cpp
@@ -63,6 +63,8 @@ void AudioConfiguration::init()
     settings()->setDefaultValue(AUDIO_DRIVER_KEY, Val("WASAPI"));
 #elif defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
     settings()->setDefaultValue(AUDIO_DRIVER_KEY, Val("ALSA"));
+#elif defined(Q_OS_MACOS)
+    settings()->setDefaultValue(AUDIO_DRIVER_KEY, Val("CoreAudio"));
 #endif
     settings()->valueChanged(AUDIO_DRIVER_KEY).onReceive(nullptr, [this](const Val&) {
         m_currentAudioDriverChanged.notify();