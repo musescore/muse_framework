7273e81fef977fa9d405d6845f9f765c6a042b6b
Dmitry Makarenko <kryksyh@gmail.com>
2026-07-28 13:02:22 +0300

    Allow creating long-lived extension script sessions
    Allows the extension instance to live across multiple function calls

--- framework/extensions/iextensionsession.h
+++ framework/extensions/iextensionsession.h
@@ -1,11 +1,8 @@
 /*
  * SPDX-License-Identifier: GPL-3.0-only
- * MuseScore-CLA-applies
+ * MuseScore/Audacity CLA applies
  *
- * MuseScore Studio
- * Music Composition & Notation
- *
- * Copyright (C) 2024 MuseScore Limited and others
+ * Copyright (C) MuseScore/Audacity and others
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -19,24 +16,28 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#ifndef MUSE_EXTENSIONS_EXTENSIONRUNNER_H
-#define MUSE_EXTENSIONS_EXTENSIONRUNNER_H
+#pragma once
 
-#include "modularity/ioc.h"
-#include "global/types/ret.h"
-#include "../extensionstypes.h"
+#include <QJSValue>
+#include <QVariant>
+
+#include "global/types/retval.h"
+
+class QObject;
 
 namespace muse::extensions {
-class ExtensionRunner
+class IExtensionSession
 {
 public:
-    ExtensionRunner(const modularity::ContextPtr& iocCtx);
+    virtual ~IExtensionSession() = default;
 
-    Ret run(const Action& action);
+    virtual Ret evaluate() = 0;
+    virtual Ret call(const QString& function, const QJSValueList& arguments = {}, QJSValue* result = nullptr) = 0;
 
-private:
-    const modularity::ContextPtr m_iocContext;
+    virtual QJSValue exports() const = 0;
+    virtual QJSValue toScriptValue(const QVariant& value) = 0;
+
+    // Keeps ownership in C++
+    virtual QJSValue wrapQObject(QObject* object) = 0;
 };
-}
-
-#endif // MUSE_EXTENSIONS_EXTENSIONRUNNER_H
+} // namespace muse::extensions
