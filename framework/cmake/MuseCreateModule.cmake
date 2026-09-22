b1b09fa0343a40f6c5a31e9663ed3ed1ab796c70
Paul MARTIN <luapmartin@gmail.com>
2026-09-22 17:18:11 +0200

    Add the per-module googletest include dir only when MUSE_ENABLE_UNIT_TESTS is ON (#268)

--- framework/cmake/MuseCreateModule.cmake
+++ framework/cmake/MuseCreateModule.cmake
@@ -82,7 +82,6 @@ function(muse_create_module target_name)
         set(MUSE_FRAMEWORK_PATH ${PROJECT_SOURCE_DIR})
     endif()
 
-    get_property(_gtest_src GLOBAL PROPERTY googletest_SOURCE_DIR)
     target_include_directories(${target_name} PRIVATE
         ${PROJECT_BINARY_DIR}
         ${CMAKE_CURRENT_BINARY_DIR}
@@ -93,7 +92,6 @@ function(muse_create_module target_name)
         ${MUSE_FRAMEWORK_PATH}
         ${MUSE_FRAMEWORK_PATH}/framework
         ${MUSE_FRAMEWORK_PATH}/framework/global
-        ${_gtest_src}/googletest/googletest/include
 
         # compat
         ${MUSE_FRAMEWORK_PATH}/src
@@ -101,6 +99,12 @@ function(muse_create_module target_name)
         ${MUSE_FRAMEWORK_PATH}/src/framework/global
         # end compat
     )
+
+    # gtest headers are reachable from any module; googletest is only fetched when unit tests are ON
+    if (MUSE_ENABLE_UNIT_TESTS)
+        get_property(_gtest_src GLOBAL PROPERTY googletest_SOURCE_DIR)
+        target_include_directories(${target_name} PRIVATE ${_gtest_src}/googletest/googletest/include)
+    endif()
 
     # Precompiled header
     if (NOT arg_NO_PCH AND MUSE_COMPILE_USE_PCH)
